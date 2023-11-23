2a1801259c924f8ad13929e0a3e6e65f7ce045a5
ch-sarfaraz <chavapatisarfaraz786@gmail.com>
2023-11-23 22:30:29 +0530

    created a new file forloop.py

---
+++ forloop.py
@@ -0,0 +1,8 @@
+num=int(input("enter the number"))
+count=1
+for i in range(1,3):
+    for j in range(10):
+        prod=j*count
+        print(num ,"*", count=" ", prod)
+        count=count+1
+     
